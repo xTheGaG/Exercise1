763139163ea248ce3b2dc8007647efb2f4eae270
xTheGaG <daniel.gegg@web.de>
2017-11-06 15:40:02 +0100

    HelloUser added

---
+++ src/HelloUser.java
@@ -0,0 +1,13 @@
+
+public class HelloUser {
+
+	String username;
+	
+	public HelloUser(String user){
+		username = user;
+	}
+	
+	public void greetUser(){
+		System.out.println("Hello");
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		System.out.println("Enter your name: ");
+		
+		Scanner reader = new Scanner(System.in);		
+		String username = reader.nextLine();
+		
+		HelloUser user = new HelloUser(username);
+		user.greetUser();
+		
+		
 	}
 
 }
